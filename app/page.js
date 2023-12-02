1ab3104e1f866f425b0341a00b55ee52b3788f7e
bvvinai <venkata.vinai@gmail.com>
2023-12-02 23:22:13 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,12 @@ import Link from "next/link";
 import UserList from "./components/userList";
 import jsonToDb from "@/libs/jsonToDb";
 import { getUsers } from "./api/users/route";
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+
+export default dynamic(() => Promise.resolve(Home), {
+  ssr: false
+})
 
 // const getUsers = async () => {
 //   try {
@@ -16,15 +22,18 @@ import { getUsers } from "./api/users/route";
 //   }
 // }
 
-export default async function Home() {
-
-  const users = await getUsers();
+async function Home() {
   //jsonToDb();
-
+  const users = await getData()
   return (
     <div>
       <Button className='bg-teal-400 px-5 py-2 m-4 rounded text-white'><Link href={"/addUser"}>Add User</Link></Button>
       <UserList userlist={users} />
     </div>
   )
+}
+
+async function getData() {
+  const res = await getUsers();
+  return res;
 }
