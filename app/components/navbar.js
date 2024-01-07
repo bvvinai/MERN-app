73f58bc190399f486ea6bd822531e405cd26d483
bvvinai <venkata.vinai@gmail.com>
2024-01-07 15:56:43 +0530

    Update navbar.js

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -6,7 +6,6 @@ export default function Navbar() {
             <div className='text-teal-400 px-2 py-2 m-4'>MERN APP</div>
             <div>
                 <Link className='bg-teal-400 px-5 py-2 m-4 rounded text-white' href={"/"}>Users</Link>
-                <Link className='bg-teal-400 px-5 py-2 m-4 rounded text-white' href={"/teams"}>Teams</Link>
             </div>
         </nav>
     )
